4a80fe3a81f0b0c2d9f1c2d929f90fb53518cb8f
Johann Sylvester J <johannsylvester04@gmail.com>
2023-10-12 20:55:07 +0530

    Fix guess check. Working now.

--- guess.cpp
+++ guess.cpp
@@ -1,26 +1,31 @@
 #include "header.hpp"
 
-bool check_guess(char guess, Word word)
+bool check_guess(char guess, Word *word)
 {
-    for (char original_ch : word.original) { // For character in original word.
+    bool guess_in_original;
+    bool already_guessed;
+    guess_in_original = already_guessed = false;
+
+    for (char original_ch : (*word).original) { // For character in original word.
         if (guess == original_ch) {
-            // For every character already guessed or displayed.
-            for (char guessed_ch : word.obfuscated) {
-                if (guess == guessed_ch) {
-                    return false;
-                }
-                else {
-                    for (int i = 0; i < word.length; i++) {
-                        if (guess == word.original[i]) {
-                            word.obfuscated[i] = guess;
-                        }
-                    }
-                }
-            }
-            return true;
+            guess_in_original = true;
+            break;
         }
-        else {
+    }
+
+    if (!guess_in_original)
+        return false;
+
+    for (char obfuscated_ch : (*word).obfuscated) {
+        if (guess == obfuscated_ch) {
             return false;
         }
     }
+
+    for (int i = 0; i < (*word).length; i++) {
+        if (guess == (*word).original[i]) {
+            (*word).obfuscated[i] = guess;
+        }
+    }
+    return false;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "header.hpp"
+#include "guess.cpp"
 #include <fstream>
 #include <vector>
 
@@ -7,6 +8,8 @@ int main()
     vector<string> words;
     ifstream words_file;
     string line;
+    char guess;
+    bool has_been_guessed;
 
     words_file.open("words.txt");
 
@@ -15,12 +18,37 @@ int main()
             words.push_back(line);
         }
     }
+
+    words_file.close();
     
     cout << "Hello. Let's play a game of hangman." << endl << endl;
 
     for (string w : words) {
         Word word(w);
-        cout << "original: " << word.original << endl;
+        has_been_guessed = false;
         cout << "obfuscated: " << word.obfuscated << endl;
+        for (int i = 0; i < 10; i++) {
+            if (word.original == word.obfuscated) {
+                cout << "\nYAY. YOU HAVE GUESSED THE WORD. LET'S GO TO THE NEXT WORD.\n\n";
+                has_been_guessed = true;
+                break;
+            }
+            cout << "\nEnter guess: ";
+            getline(cin, line);
+            if (line.length() > 1 || line.length() == 0) {
+                cout << "INVALID INPUT.";
+                i--;
+                continue;
+            }
+            else {
+                guess = line[0];
+            }
+            check_guess(guess, &word);
+            cout << "obfuscated: " << word.obfuscated << endl;
+        }
+        if (!has_been_guessed)
+            cout << "\nYOU'VE RUN OUT OF CHANCES. WORD NOT GUESSED. MOVING ON.\n\n";
     }
+
+    cout << "All words guessed. Congratulations!";
 }
